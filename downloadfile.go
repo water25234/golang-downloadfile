ffb09d02b962d7e46191de9d1bd3f78a7cee5986
water25234 <water25234@gmail.com>
2020-07-10 21:27:11 +0800

    optimization code, array change to  slice code

--- downloadfile.go
+++ downloadfile.go
@@ -1,37 +1,39 @@
 package main
 
 import (
-    "fmt"
-    "io"
-    "net/http"
-    "net/url"
-    "os"
-    "strings"
-    "time"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+	"time"
 )
 
 var (
-    fileName    string
-    fullUrlFile string
-    theArray [5]string
+	fileName    string
+	fullUrlFile string
+	theArray    []string
 )
 
 func theArrayList() {
-    theArray[0] = "http://www.golangprograms.com/skin/frontend/base/default/logo.jpg"
-    theArray[1] = "https://golangcode.com/images/avatar.jpg"
+	theArray = []string{
+		"http://www.golangprograms.com/skin/frontend/base/default/logo.jpg",
+		"https://golangcode.com/images/avatar.jpg",
+	}
 }
 
 func main() {
 
-    theArrayList()
+	theArrayList()
 
 	for i := 0; i < len(theArray); i++ {
 
-        if i%100 == 0 && i > 0 {
-            fmt.Println("機器累了休息中")
-            time.Sleep(time.Duration(2)*time.Second)
-            fmt.Println("機器上工囉～")
-        }
+		if i%100 == 0 && i > 0 {
+			fmt.Println("機器累了休息中")
+			time.Sleep(time.Duration(2) * time.Second)
+			fmt.Println("機器上工囉～")
+		}
 
 		fullUrlFile = theArray[i]
 		// Build fileName from fullPath
@@ -46,57 +48,57 @@ func main() {
 }
 
 func putFile(file *os.File, client *http.Client) {
-    resp, err := client.Get(fullUrlFile)
+	resp, err := client.Get(fullUrlFile)
 
-    checkError(err)
+	checkError(err)
 
-    defer resp.Body.Close()
+	defer resp.Body.Close()
 
-    size, err := io.Copy(file, resp.Body)
+	size, err := io.Copy(file, resp.Body)
 
-    defer file.Close()
+	defer file.Close()
 
-    checkError(err)
+	checkError(err)
 
-    fmt.Println("Just Downloaded a file", fileName, "with size", size)
+	fmt.Println("Just Downloaded a file", fileName, "with size", size)
 }
 
 func buildFileName() {
-    fileUrl, err := url.Parse(fullUrlFile)
-    checkError(err)
+	fileUrl, err := url.Parse(fullUrlFile)
+	checkError(err)
 
-    path := fileUrl.Path
-    segments := strings.Split(path, "/")
+	path := fileUrl.Path
+	segments := strings.Split(path, "/")
 
-    fileName = segments[len(segments)-1]
+	fileName = segments[len(segments)-1]
 
-    var fileNameSplit = strings.Split(fileName, ".")
+	var fileNameSplit = strings.Split(fileName, ".")
 
-    if len(fileNameSplit) == 1 {
-        fileName = strings.Join([]string{fileName,"png"}, ".")
-    }
+	if len(fileNameSplit) == 1 {
+		fileName = strings.Join([]string{fileName, "png"}, ".")
+	}
 }
 
 func httpClient() *http.Client {
-    client := http.Client{
-        CheckRedirect: func(r *http.Request, via []*http.Request) error {
-            r.URL.Opaque = r.URL.Path
-            return nil
-        },
-    }
+	client := http.Client{
+		CheckRedirect: func(r *http.Request, via []*http.Request) error {
+			r.URL.Opaque = r.URL.Path
+			return nil
+		},
+	}
 
-    return &client
+	return &client
 }
 
 func createFile() *os.File {
-    file, err := os.Create(fileName)
+	file, err := os.Create(fileName)
 
-    checkError(err)
-    return file
+	checkError(err)
+	return file
 }
 
 func checkError(err error) {
-    if err != nil {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 }
